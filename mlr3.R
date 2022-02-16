3b07fcfadb61b6d9ee6d6f13691e88b8f383c082
yeonhoon <aidenjang94@gmail.com>
2022-02-16 20:29:52 +0900

    learning

--- mlr3.R
+++ mlr3.R
@@ -1,26 +1,36 @@
-require(tidyverse)
-install.packages('tidymodels')
 
-require(tidymodels)
-
-install.packages('caret')
-
-require(mlr3)
+require(mlr3verse)
 require(data.table)
 
-# Object creation: <Class>$new()
+# Object creation: <Class>$new() or mlr3::tsk()
 penguins <- TaskClassif$new(id='penguins',
                             backend=palmerpenguins::penguins,
                             target='species')
 task <- tsk('iris')
+task2 <- TaskClassif$new(id='iris',
+                         backend = iris,
+                         target='Species')
 
-penguins$ncol
-penguins$feature_names
-penguins$target_names
-penguins$truth(rows = 200)
-penguins$data(rows = 200, cols='sex')
-penguins$select(cols='island') # 특정 feature 선택
-penguins$filter(rows=1:10)
+
+
+# pre-defined datasets in mlr3
+
+mlr_tasks
+as.data.table(mlr_tasks) #pxc: POSIXct
+
+
+# task object로 할 수 있는 기능들
+task$ncol
+task$feature_names # feature 이름
+task$class_names # target의 class 이름
+task$target_names
+task$truth(rows = 200)
+task$row_ids
+task$data(rows = 150, cols='Species')
+task$select(cols='island') # 특정 feature 선택
+task$filter(rows=1:10) # rows filter
+task$task_type
+task$col_roles
 
 
 # Dictionaries -----------------------------------------------------------
@@ -31,30 +41,100 @@ penguins$filter(rows=1:10)
 # Resampling mlr_resamplings => rsml()
 
 
-# learner: training model
-learner <-  lrn("classif.rpart", cp=.01)
-as.data.table(learner$param_set)
+require(mlr3)
+mlr_learners
+?mlr_measures
+mlr3misc::dictionary_sugar(tsk())
 
+# EDA ---------------------------------------------------------------------
 
-# training ---------------------------------------------------------------
-train_set <- sample(x = task$nrow, size = 0.8 * task$nrow)
-test_set <- setdiff(seq_len(task$nrow), train_set)
+summary(as.data.table(task))
+mlr3viz::autoplot(task)
+mlr3viz::autoplot(task, type='pairs')
+mlr3viz::autoplot(task, type='duo')
 
+# learner for training ---------------------------------------------------------------
 
+mlr3misc::dictionary_sugar(lrn()) 
+mlr_learners
+#classif.featureless
+lnr('')
+
+data('Sonar', package = 'mlbench')
+task <- as_task_classif(Sonar, target='Class', positive="M")
+learner <-  lrn("classif.rpart", cp=.01, predict_type='prob')
+
+# learner$param_set$values$cp <- 0.02
+# as.data.table(learner$param_set)
 # hyperparameter 변경
 learner$param_set$values = list(maxdepth=1, xval=0)
 
 learner$train(task, row_ids = train_set)
 
-#prediction --------------------------------------------
+
+
+
+# Thresholding ----------------------------------------------------------------
+
+pred <- learner$train(task)$predict(task)
+mlr3misc::dictionary_sugar_get(msr())
+measures <- msrs(c('classif.tpr','classif.tnr','classif.acc'))
+pred$confusion
+pred$score(measures)
+
+pred$set_threshold(0.2)
+pred$confusion
+pred$score(measures)
+
+
+# Train, Prediction, and Assessment ---------------------------------------
+
+task <- tsk('penguins')
+learner <- lrn('classif.rpart')
+
+# train_test_split
+
+train_set <- sample(task$nrow, 0.8*task$nrow)
+test_set <- setdiff(seq_len(task$nrow), train_set)
+
+# train the learner
+
+learner$model
+learner$train(task, row_ids=train_set)
+
+# prediction 
 
 prediction <- learner$predict(task, row_ids = test_set)
+prediction
 
 prediction$confusion
-prediction$score()
 
+# changing the predict type: acc to prob
+
+learner$predict_type <- 'prob'
+prediction <- learner$predict(task, row_ids = test_set)
+prediction$prob
+prediction$response
+
+autoplot(prediction)
 measure <- msr('classif.acc') # accuracy
 prediction$score(measure)
+
+
+# ROC ---------------------------------------------------------------------
+# data('Sonar',package = "mlbench")
+task <- as_task_classif(Sonar,target='Class',positive='M')
+leaner <- lrn('classif.rpart', predict_type='prob')
+pred <- learner$train(task)$predict(task)
+pred$set_threshold(0.1)
+CM <- pred$confusion
+CM
+
+roc_curve <- autoplot(pred, type='roc')
+prc_curve <- autoplot(pred, type='prc')
+
+roc_curve
+
 
 # Resampling: CV ------------------------------------------------------------
 
