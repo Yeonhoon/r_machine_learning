5a9db949a810e6be600067ea22aad9c4ea82efd0
yeonhoon <aidenjang94@gmail.com>
2022-02-18 20:23:18 +0900

    resampling

--- mlr3.R
+++ mlr3.R
@@ -122,9 +122,9 @@ prediction$score(measure)
 
 
 # ROC ---------------------------------------------------------------------
-# data('Sonar',package = "mlbench")
+data('Sonar',package = "mlbench")
 task <- as_task_classif(Sonar,target='Class',positive='M')
-leaner <- lrn('classif.rpart', predict_type='prob')
+learner <- lrn('classif.rpart', predict_type='prob')
 pred <- learner$train(task)$predict(task)
 pred$set_threshold(0.1)
 CM <- pred$confusion
@@ -137,13 +137,90 @@ roc_curve
 
 
 # Resampling: CV ------------------------------------------------------------
+# 종류
+# cv, loo(leave one out), repeated_cv, 
+# bootstrapping, subsampling, holdout, insampling, custom
 
-cv5=rsmp('cv', folds=5)
+as.data.table(mlr_resamplings)
 
-rr = resample(task, learner, cv5)
+resampling <- rsmp('cv',folds=5)
+# resampling$param_set$values <- list(ratio=.8)
+print(resampling)
+
+resampling$instantiate(task)
+str(resampling$train_set(1))
+str(resampling$test_set(1))
+
+rr = resample(task, learner, resampling, store_models = T)
+print(rr)
 rr$score(measure)
 
-rr_table = as.data.table(rr)
+# cv한 이후의 measures
+rr$aggregate(msr('classif.ce'))
 
-# resample ==> aggregate
+# visualization
+autoplot(rr, measure = msr('classif.auc'))
+autoplot(rr, type='roc')
+rr$filter(1)
+autoplot(rr, type = "prediction")
+
+
+# benchmarking ------------------------------------------------------------
+
+# Comparing the performance of different learners
+
+design <- benchmark_grid(
+  task = tsks(c('spam','german_credit','sonar')),
+  learners = lrns(c('classif.ranger','classif.rpart','classif.featureless'),
+                  predict_type = 'prob', predict_sets= c('train','test')),
+  resamplings = rsmps('cv',folds=5)
+)
+print(design)
+
+bmr <- benchmark(design)
+
+measures <- list(
+  msr('classif.auc',predict_sets='train', id='auc_train'),
+  msr('classif.auc',id='auc_test')
+)
+tab <- bmr$aggregate(measures)
+
+rank <- tab[,.(learner_id, rank_train = rank(-auc_train),
+               rank_test = rank(-auc_test)),by=task_id]
+ranks <- rank[,.(mrank_train = mean(rank_train), mrank_test = mean(rank_test)),by=learner_id]
+
+ranks[order(mrank_test)]
+
+# Plotting
+require(ggplot2)
+autoplot(bmr) + ggplot2::theme(axis.text.x = ggplot2::element_text(angle=45, hjust=1))
+bmr_german <- bmr$clone()$filter(task_id = 'german_credit')
+bmr_sonar <- bmr$clone()$filter(task_id = 'sonar')
+autoplot(bmr_german, type='roc') + theme(legend.position = 'bottom')
+autoplot(bmr_sonar, type='roc') + theme(legend.position = 'bottom')
+
+# extracting resample results
+
+tab <- bmr$aggregate(measures)
+rr <- tab[learner_id =='classif.ranger']$resample_result[[1]]
+
+measure <- msr('classif.auc')
 rr$aggregate(measure)
+
+
+# converting and merging --------------------------------------------------
+
+# resampling 모델들을 benchmark result로 converting 한 뒤, 
+# combine()을 활용하여 merge 가능
+
+task <- tsk('iris')
+resampling <- rsmp('cv')$instantiate(task)
+
+rr1 <- resample(task,lrn('classif.rpart'),resampling)
+rr2 <- resample(task,lrn('classif.featureless'),resampling)
+
+bmr1 <- as_benchmark_result(rr1)
+bmr2 <- as_benchmark_result(rr2)
+
+bmr1
+bmr1$combine(bmr2)
